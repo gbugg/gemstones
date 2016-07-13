84c42fb35e2d2b9e1b0821405b742a2360119636
gmbugg <greg.bugg@bridges-inc.com>
2016-07-13 05:23:04 +0000

    added lapis changes

--- gemstones/settings.py
+++ gemstones/settings.py
@@ -103,4 +103,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= "/home/gmbugg/gemstones/jade/static/"
+STATIC_ROOT= "/home/gmbugg/gemstones/lapis/static/"
